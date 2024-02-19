31aee695e28378735320d879baecc656982a6264
Arantzazul <arantzazuzabalza@gmail.com>
2024-02-19 13:38:17 +0100

    First working version

--- plot_routes.py
+++ plot_routes.py
@@ -1,8 +1,9 @@
+import csv
 import googlemaps
+import io
 from datetime import datetime
+from pathlib import Path
 from PyQt5.QtCore import QVariant
-
-
 
 
 def decode(point_str):
@@ -68,54 +69,95 @@ def decode(point_str):
         # they were encoded
         points.append((round(prev_x, 6), round(prev_y, 6)))
 
-    print(points)
+    # print(points)
     return points
 
 class PlotRoutes:
     def __init__(self):
         self.gmaps = googlemaps.Client("AIzaSyCvJVZR8eK-yi8HDVfcF2VSD9xrYRDzczg")
+        self.building_address = {"ATEGORRIETA (LH)": "Atarizar Kalea, 18, 20013 Donostia, Gipuzkoa",
+                                 "VILLA SOROA (HH)": "Ategorrieta Hiribidea, 24, 20013 Donostia, Gipuzkoa",
+                                 "HURRA (DBH eta BATXILERGOA)": "Indianoene Kalea, 1, Donostia"}
+        self.modes = {"Oinez / A pie": "walking",
+                      "Autoz / En coche": "driving",
+                      "Bizikletaz / En bici": "bicycling",
+                      "Autobusez / En autobús": "transit",
+                      "Motorrez / En moto": "driving",
+                      "Patinetez / En patinete": "bicycling"}
 
     def get_qgs_feature_from_directions(self, from_address, to_address, mode, departure_time):
+        print(f'Mode: {mode}\n')
         directions_result = self.gmaps.directions(from_address, to_address, mode=mode,
                                                   departure_time=departure_time)
         point_str = str(directions_result[0]["overview_polyline"]["points"])
         points = decode(point_str)
 
-        fet = QgsFeature()
+        if len(points) <= 0:
+            print(f'No points for route from: {from_address}\n\tto: {to_address}\n\tmode: {mode}')
+
+        feature = QgsFeature()
         seg = []
         for i in range(0, len(points)):
             seg.append(QgsPoint(points[i][0], points[i][1]))
 
-        fet.setGeometry(QgsGeometry.fromPolyline(seg))
-        fet.setAttributes(["route provided by google maps api"])
+        feature.setGeometry(QgsGeometry.fromPolyline(seg))
+        feature.setAttributes(["route provided by google maps api"])
 
-        return fet
+        return feature
 
     def get_addresses_from_file(self, path):
         # TODO import from csv
+        # returns a list of tuples (from_address, to_address)
+        addresses = []
+        with io.open(path, encoding='latin1', newline='') as csv_file:
+        # with open(path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            column_names = None
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    print(f'Column names are {", ".join(row)}')
+                    line_count += 1
+                    column_names = row
+                else:
+                    building = row[column_names.index("Building")]
+                    address = row[column_names.index("Address")]
+                    city = row[column_names.index("City")]
+                    mode_in = row[column_names.index("Way in")]
+                    mode_in = self.modes[mode_in]
+                    # print(f'\t Building: {building}, Address: {address}, City: {city}.')
+                    addresses.append((address + ", " + city, self.building_address[building], mode_in))
+                    line_count += 1
+            print(f'Processed {line_count} lines.')
+            return addresses
 
-    def plot_routes_from_file(self, path, mode, departure_time):
+    def plot_routes_from_file(self, path, departure_time):
+        # Create qgis layer
+        layer = QgsVectorLayer('LineStringZ', 'route', "memory")
+        pr = layer.dataProvider()
+        pr.addAttributes([QgsField("attribution", QVariant.String)])
+        layer.updateFields()
+
         addresses = self.get_addresses_from_file(path)
         for address in addresses:
-            fet = get_qgs_feature_from_directions(address[0], address[1], mode, departure_time)
+            feature = self.get_qgs_feature_from_directions(address[0], address[1], address[2], departure_time)
+            pr.addFeatures([feature])
 
-# TODO maybe use a more specific time
-departure_time = datetime.now()
+        layer.updateExtents()  # update it
+        QgsProject.instance().addMapLayer(layer)
 
-
-
-layer = QgsVectorLayer('LineStringZ', 'route', "memory")
-pr = layer.dataProvider()
-pr.addAttributes([QgsField("attribution", QVariant.String)])
-layer.updateFields()
-
-pr.addFeatures([fet])
-layer.updateExtents()  # update it
-QgsProject.instance().addMapLayer(layer)
 
 def main():
+    print("Running plot routes")
+    # TODO maybe use a more specific time
+    departure_time = datetime.now()
+    data_folder = Path("C:/DATOS/PROYECTOS/2303_Mugikortasun batzordea/QGIS/Input data/Familiak joan etorriak Inkesta 2022")
+    file_path = data_folder / "Familiak 2022 MUGIKORTASUN PLANA_ IKASTOLARAKO JOAN- ETORRIAK (Erantzunak)-FOR QGIS-City added.csv"
+    # file_path = 'C:\DATOS\PROYECTOS\2303_Mugikortasun batzordea\QGIS\Input data\Familiak joan etorriak Inkesta 2022\Familiak 2022 MUGIKORTASUN PLANA_ IKASTOLARAKO JOAN- ETORRIAK (Erantzunak)-FOR QGIS-City added.csv'
     plot_routes = PlotRoutes()
+    plot_routes.plot_routes_from_file(file_path, departure_time)
 
+main()
 
 if __name__ == "__main__":
     main()
